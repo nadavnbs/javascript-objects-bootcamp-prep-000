327cd3f4a10818ca245a7255fad4151a07deba9b
nadav ben shimon <nadavnbs@gmail.com>
2018-03-19 15:00:25 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,8 @@ function updatePlaylist(obj,artistName,songTitle){
   obj[artistName] = songTitle
   return obj
 }
+
+function removeFromPlaylist (obj,artistName){
+  delete obj.artistName
+  return obj
+}
